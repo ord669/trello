a779e7e88e32ccc184d68c3efe4be5449935581c
Oren Sharizad <oren.sharizad12@gmail.com>
2023-01-17 23:41:41 +0200

    svg added to our files

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -2,29 +2,12 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import logo from '../assets/img/logo.png'
 import { CHANGE_COUNT } from '../store/user.reducer'
-
 export function HomePage() {
-    const dispatch = useDispatch()
-    const count = useSelector(storeState => storeState.userModule.count)
-
-    function changeCount(diff) {
-        console.log('Changing count by:', diff);
-        dispatch({ type: CHANGE_COUNT, diff })
-    }
 
     return (
         <section>
-            <h1>Hello World to all</h1>
-            <img src={logo} alt="Logo" style={{ maxWidth: '300px' }} />
-            <h2>
-                Count {count}
-                <button onClick={() => {
-                    changeCount(1)
-                }}>+</button>
-                <button onClick={() => {
-                    changeCount(10)
-                }}>+10</button>
-            </h2 >
+            <h1>Hello from home page</h1>
+
         </section >
     )
 }
