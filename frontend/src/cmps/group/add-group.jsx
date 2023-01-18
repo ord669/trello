bdff51f1de0156797239cd9d7c811f890806b6bc
ord669 <ord24@gmail.com>
2023-01-18 20:48:22 +0700

    add Btn

--- frontend/src/cmps/group/add-group.jsx
+++ frontend/src/cmps/group/add-group.jsx
@@ -1,7 +1,11 @@
+import { PlusIcon } from "../../assets/svg/icon-library";
+
 export function AddGroup() {
     return (
-        <section className='add-group'>
-            add-group
+        <section className='group-preview add-group'>
+            <PlusIcon />
+            <h3>Add a Card</h3>
+
         </section>
     )
 }
