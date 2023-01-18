d373b572baa7dacf8933183cf60b6cb91ff8e3d2
Oren Sharizad <oren.sharizad12@gmail.com>
2023-01-18 20:43:25 +0200

    test

--- frontend/src/views/task-details.jsx
+++ frontend/src/views/task-details.jsx
@@ -1,24 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { ChecklistIcon, CloseIcon, DescriptionIcon, LabelIcon, ManIcon } from "../assets/svg/icon-library";
+import { loadBoard } from "../store/board/board.action";
 
 export function TaskDetails() {
     const { board } = useSelector(storeState => storeState.boardModule)
     const { taskId, groupId } = useParams()
     const [labels, setLabels] = useState([])
+    const [group, setGroup] = useState([])
+    const [task, setTask] = useState({})
 
-    console.log('board: ', board);
     console.log('groupId: ', groupId);
     console.log('taskId: ', taskId);
+    console.log('board: ', board);
+    console.log('board.groups:', board.groups)
+
+    useEffect(() => {
+        if (!board.groups) return
+        loadTask()
+    }, [board])
+
+    function loadTask() {
+        const currGroup = board.groups.find(group => group.id === groupId)
+        console.log('currGroup: ', currGroup);
+        const currTask = currGroup.tasks.find(task => task.id === taskId)
+        setTask(currTask)
+    }
 
     return (
         <section className='task-details'>
             <div className="black-screen"></div>
             <div className="main-task-details">
                 <button className=" btn details-close-btn"><CloseIcon /></button>
-                <div className="task-details-cover">cover</div>
-                <div className="task-details-title">title</div>
+                <div className="task-details-cover full-task"></div>
+                <div className="task-details-title">{task.title}</div>
                 <div className="task-details-content">
                     <div className="task-details-labels">
                         <h1>labels</h1>
@@ -26,9 +42,9 @@ export function TaskDetails() {
                     <div className="task-details-description">
                         <div className="flex">
                             <DescriptionIcon />
-                            <h1>Description</h1>
+                            <h1>{task.description}</h1>
                         </div>
-                        <input type="text" />
+                        <textarea type="text" />
                     </div>
                     <div className="task-details-checklist">
                         <h1>CheckList</h1>
