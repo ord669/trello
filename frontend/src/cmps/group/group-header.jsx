f795472fcf3ee8854e1e16ed949681d52f58074e
liad7 <liadgola7@gmail.com>
2023-01-19 22:35:53 +0200

    group header edit and style

--- frontend/src/cmps/group/group-header.jsx
+++ frontend/src/cmps/group/group-header.jsx
@@ -1,7 +1,35 @@
-export function GroupHeader() {
+import { useState } from "react"
+import { showErrorMsg, showSuccessMsg } from "../../services/event-bus.service"
+import { saveGroup } from "../../store/board/board.action"
+
+export function GroupHeader({ onRemoveGroup, group }) {
+    const [title, setTitle] = useState(group.title)
+
+    function handleChange({ target }) {
+        setTitle(target.value)
+    }
+
+    async function onSaveTitle() {
+        const newTitle = title.trimEnd()
+        if (newTitle === group.title) return
+        try {
+            const groupToSave = { ...group, title: newTitle }
+            await saveGroup(groupToSave)
+            showSuccessMsg('Changes saved')
+        } catch (err) {
+            showErrorMsg('Cannot update title, try later')
+        }
+    }
+
     return (
         <section className='group-header'>
-            group-header
+            <textarea
+            className="group-title edit-title-input"
+            onFocus={(ev)=>ev.target.select()}
+                onBlur={onSaveTitle}
+                onChange={handleChange}
+                value={title} />
+            <button className="btn-remove-group" onClick={onRemoveGroup}>X</button>
         </section>
     )
 }
--- frontend/src/cmps/group/group-preview.jsx
+++ frontend/src/cmps/group/group-preview.jsx
@@ -2,6 +2,7 @@ import { TaskList } from "../task/task-list"
 import { AddTask } from "../task/add-task"
 import { removeGroup } from "../../store/board/board.action"
 import { showErrorMsg, showSuccessMsg } from "../../services/event-bus.service"
+import { GroupHeader } from "./group-header"
 
 export function GroupPreview({ group }) {
 
@@ -15,11 +16,11 @@ export function GroupPreview({ group }) {
     }
     return (
         <section className='group-preview'>
-            <section className="group-preview-header">
+            {/* <section className="group-preview-header">
                 <p>{group.title}</p>
                 <button className="btn-remove-group" onClick={onRemoveGroup}>X</button>
-            </section>
-            {/* <h2>{group.title}</h2> */}
+            </section> */}
+            <GroupHeader onRemoveGroup={onRemoveGroup} group={group} />
             <TaskList tasks={group.tasks} />
 
             <AddTask groupId={group._id} />
