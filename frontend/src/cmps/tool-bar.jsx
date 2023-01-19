4d841326b9217b014eed3c26612329f4d450c2b3
ord669 <ord24@gmail.com>
2023-01-19 14:21:56 +0700

    removed filter button

--- frontend/src/cmps/tool-bar.jsx
+++ frontend/src/cmps/tool-bar.jsx
@@ -5,12 +5,12 @@ export function ToolBar({ board }) {
     return (
         <section className='tool-bar full'>
             <h2>{board.title}</h2>
-            <button className="btn-header">
+            {/* <button className="btn-header">
                 <div className="icon">
                     <FilterIcon />
                 </div>
                 <p>Filter</p>
-            </button>
+            </button> */}
         </section>
     )
 }
