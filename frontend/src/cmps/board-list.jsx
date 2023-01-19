6b61f7619c2a2eeb91a5fe61bd62536590b56125
liad7 <liadgola7@gmail.com>
2023-01-19 23:12:06 +0200

    hover on add task

--- frontend/src/cmps/board-list.jsx
+++ frontend/src/cmps/board-list.jsx
@@ -3,7 +3,7 @@ import { BoardPreview } from "./board-preview"
 export function BoardList({ boards, onCreateBoard,onRemoveBoard }) {
     return (
         <section className='board-list'>
-            {boards.length && boards.map(board => <BoardPreview key={board._id} board={board} onRemoveBoard={onRemoveBoard}/>)}
+            {!!boards.length && boards.map(board => <BoardPreview key={board._id} board={board} onRemoveBoard={onRemoveBoard}/>)}
             <section className="add-board" onClick={onCreateBoard}>Create new board</section>
         </section>
     )
