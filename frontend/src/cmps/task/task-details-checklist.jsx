0114048dbf3ad896fcdf9ef2f53961196b5a5c48
Oren Sharizad <oren.sharizad12@gmail.com>
2023-01-19 13:28:47 +0200

    task checlist added - Oren

--- frontend/src/cmps/task/task-details-checklist.jsx
+++ frontend/src/cmps/task/task-details-checklist.jsx
@@ -1,18 +1,38 @@
 import { ChecklistIcon } from "../../assets/svg/icon-library";
 
 
-export function TaskDetailsChecklist() {
+export function TaskDetailsChecklist({ checklists }) {
+    console.log('checklists: ', checklists);
+
+    if (!checklists) return
     return (
         <section className='task-details-checklist'>
-            <div className="flex align-center gap-10">
-                <ChecklistIcon className='icon-title' />
-                <h1>CheckList</h1>
+            <div className="task-checklists">
+                {checklists.map(checklist =>
+                    <div key={checklist._id}>
+                        <div className="flex align-center gap-10">
+                            <div className=" icon-title">
+                                <ChecklistIcon />
+                            </div>
+                            <h1>{checklist.title}</h1>
+                        </div>
+                        <div className="task-todos">
+                            {checklist.todos.map(todo =>
+                                <div key={todo._id}>
+                                    <input className="icon-title"
+                                        type="checkbox"
+                                        defaultChecked={todo.isDone}
+                                    />
+                                    <div className={todo.isDone ? 'line-through ' : ''}>
+                                        {todo.title}
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                )
+                }
             </div>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-        </section>
+        </section >
     )
 }
