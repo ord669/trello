c1603bbc76ae4784641744e9465e4a61722ba4a2
ord669 <ord24@gmail.com>
2023-01-21 12:56:09 +0700

    added label

--- frontend/src/cmps/task/task-preview/task-preview-mini-labels-list.jsx
+++ frontend/src/cmps/task/task-preview/task-preview-mini-labels-list.jsx
@@ -3,7 +3,7 @@ import { TaskPreviewLabels } from "./task-preview-mini-labels"
 export function MiniLabelList({ task, onSelectLable }) {
     return (
         <section >
-            <p>Labels</p>
+            {/* <p>Labels</p> */}
             <div className="mini-labels-list-container">
                 {task.labelIds.map((labelId, idx) =>
                     <div onClick={() => { onSelectLable(labelId) }} key={idx}>
