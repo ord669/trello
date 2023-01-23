b1c92a708f8c25005b20439c125b9ced398bd1e5
liad7 <liadgola7@gmail.com>
2023-01-23 16:54:51 +0200

    comment list

--- frontend/src/cmps/board-activity.jsx
+++ frontend/src/cmps/board-activity.jsx
@@ -1,40 +1,11 @@
-import { utilService } from "../services/util.service";
-import { useNavigate } from 'react-router-dom'
-
+import { ActivityPreview } from "./activity-preview"
 
 export function BoardActivity({ board }) {
-    const navigate = useNavigate()
-
-    function onNavigate(task) {
-        navigate(`/board/${board._id}/${task.groupId}/${task._id}`)
-
-    }
-
-
-
     return (
-        <section className='board-activities'>
+        <section className='board-activity'>
             {board.activities.map(activity =>
-                <div className="board-activity-container" key={activity._id}>
-                    <div className="activity-img "
-                        style={{ backgroundImage: `url(${activity.byMember.imgUrl})` }}>
-                    </div>
-                    <div className="activity-content ">
-
-                        <p>
-                            <span className="activity-fullname">
-                                {activity.byMember.fullname}
-                            </span>
-                            {activity.txt}
-                            <span onClick={() => onNavigate(activity.task)} className="activity-task">{activity.task.title} </span>
-                        </p>
-                        <p className="activity-time">{utilService.formatTime(activity.createdAt)}</p>
-
-                    </div>
-                </div>
-
-            )
-            }
+                <ActivityPreview key={activity._id} activity={activity} />
+            )}
         </section >
     )
 }
