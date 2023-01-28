7edd6e747453d3ea6c0c59bbe6df8e49b04979a8
Oren Sharizad <oren.sharizad12@gmail.com>
2023-01-28 12:55:44 +0200

    labels are changes the size when you click now

--- frontend/src/cmps/task/task-preview/task-preview-mini-labels-list.jsx
+++ frontend/src/cmps/task/task-preview/task-preview-mini-labels-list.jsx
@@ -1,12 +1,12 @@
 import { TaskPreviewLabels } from "./task-preview-mini-labels"
 
-export function MiniLabelList({ task, onSelectLabel }) {
+export function MiniLabelList({ task, onSelectLabel, type }) {
     return (
         <section>
             <div className="mini-labels-list-container">
                 {task.labelIds.map((labelId, idx) =>
                     <div onClick={() => { onSelectLabel(labelId) }} key={idx}>
-                        <TaskPreviewLabels labelId={labelId} />
+                        <TaskPreviewLabels type={type} labelId={labelId} />
                     </div>)}
             </div>
         </section>
