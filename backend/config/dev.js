e01b59de61c30d8085eacd9a616ae21995ea2dcb
ord669 <ord24@gmail.com>
2023-01-29 12:58:30 +0700

    added api key

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   // dbURL: 'mongodb://127.0.0.1:27017',
   // dbName: 'trello_db'
   dbURL: 'mongodb+srv://oren:oren123@cluster0.nswgy6d.mongodb.net/?retryWrites=true&w=majority',
-  dbName: 'trello_db_atlas'
+  dbName: 'trello_db_atlas',
+  OPENAI_API_KEY: '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'
 
 }
